525ed15f08ba8b71d191dd3064c0fbc1469370d5
Andrey Maslennikov <mandrey@microsoft.com>
2020-10-14 08:38:00 +0300

    Win10 setup using winget (pilot)

---
+++ win10/init.ps1
@@ -0,0 +1,11 @@
+Enable-WindowsOptionalFeature -Online -NoRestart -All -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -NoRestart -All -FeatureName VirtualMachinePlatform
+Enable-WindowsOptionalFeature -Online -NoRestart -All -FeatureName Microsoft-Hyper-V
+
+Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile wsl.msi
+.\wsl.msi /quiet
+
+wsl --set-default-version 2
+
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
+Add-AppxPackage .\Ubuntu.appx
---
+++ win10/win10-software.bat
@@ -0,0 +1,16 @@
+:: general dev tools
+:: interactive is required to enable explorer integration
+winget install --interactive vscode
+winget install --silent "Windows Terminal"
+winget install --silent Git
+winget install --silent "Docker Desktop"
+winget install --silent "GitHub Desktop"
+
+:: Microsoft
+winget install --silent "Visual Studio Enterprise"
+
+:: other productivity (or not so much) tool
+winget install --silent "Telegram Desktop"
+winget install --silent powertoys
+winget install --silent 7zip
+winget install --silent "Adobe Acrobat Reader DC"
